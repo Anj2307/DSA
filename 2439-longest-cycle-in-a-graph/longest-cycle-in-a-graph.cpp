f6abb8c88cacdc0aed719e4236f8aaca8f170955
Anj2307 <mc24bt005@iitdh.ac.in>
2025-07-01 19:15:54 +0530

    Time: 267 ms (21.81%) | Memory: 202.2 MB (14.72%) - LeetSync

---
+++ 2439-longest-cycle-in-a-graph/longest-cycle-in-a-graph.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestCycle(vector<int>& edges) {
+        int n = edges.size();
+        vector<bool> visited(n, false);
+        vector<int> depth(n, -1);
+        int maxCycle = -1;
+
+        for (int i = 0; i < n; i++) {
+            if (visited[i]) continue;
+
+            int node = i;
+            unordered_map<int, int> localDepth;
+            int d = 0;
+
+            // Traverse the path starting from node
+            while (node != -1 && !visited[node]) {
+                visited[node] = true;
+                localDepth[node] = d++;
+                node = edges[node];
+            }
+
+            // If we encounter a node in localDepth again, we found a cycle
+            if (node != -1 && localDepth.count(node)) {
+                maxCycle = max(maxCycle, d - localDepth[node]);
+            }
+        }
+
+        return maxCycle;
+    }
+};
