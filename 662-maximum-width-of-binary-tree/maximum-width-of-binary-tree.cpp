5be0162b05daaa5b698af1e144f8d6a167f0a4b9
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-03-14 16:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (43.08%) - LeetSync

---
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+
+        if(!root) return 0;
+
+        long long ans = 0;
+
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root,0});
+
+        while(!q.empty()){
+
+            int size = q.size();
+            long long left = q.front().second;
+            long long right = left;
+
+            for(int i=0;i<size;i++){
+
+                auto u = q.front();
+                q.pop();
+
+                TreeNode* node = u.first;
+                long long idx = u.second - left; 
+
+                right = idx;
+
+                if(node->left)
+                    q.push({node->left,2*idx});
+
+                if(node->right)
+                    q.push({node->right,2*idx+1});
+            }
+
+            ans = max(ans, right+1);
+        }
+
+        return ans;
+    }
+};
