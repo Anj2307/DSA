d7573dc7de00cf96a6174fd506b8fe0279f733a6
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-06-19 17:55:15 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (58.90%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,21 +1,25 @@
 class Solution {
-    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int c) {
-        if(sr < 0 || sr >= image.size() || sc < 0 || sc >= image[0].size()) return;
-        if(image[sr][sc] != c || image[sr][sc] == color) return;
-
-        image[sr][sc] = color;
-
-        dfs(image, sr + 1, sc, color, c);
-        dfs(image, sr - 1, sc, color, c);
-        dfs(image, sr, sc + 1, color, c);
-        dfs(image, sr, sc - 1, color, c);
-    }
-
 public:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color, int p){
+        
+        if (sr < 0 || sc < 0 || sr >= image.size() || sc >= image[0].size()) {
+            return;
+        }
+        if (image[sr][sc] == p) {
+            image[sr][sc] = color;
+            dfs(image, sr - 1, sc, color, p);
+            dfs(image, sr, sc - 1, color, p);
+            dfs(image, sr + 1, sc, color, p);
+            dfs(image, sr, sc + 1, color, p);
+        }
+    }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-        if(originalColor != color)
-            dfs(image, sr, sc, color, originalColor);
+        if (image[sr][sc] == color) {
+            return image;
+        }
+        
+        dfs(image, sr, sc, color, image[sr][sc]);
         return image;
     }
 };
