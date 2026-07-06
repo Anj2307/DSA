f279b72a4eb2a94339dcf78ed20db54faecbbbb8
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-07-06 14:14:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (8.09%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+
+        // leave first
+        vector<int>dp(nums.size()-1,0);
+
         if(nums.size()==1) return nums[0];
-        vector<int>dp(nums.size()-1,-1);
-         dp[0]=nums[0];
-         if(nums.size()==1) return nums[0];
-        for(int i=1;i<nums.size()-1;i++)
-        {
-            int a=0;
-            if(i<2) a=0;
-            else a=dp[i-2];
-            dp[i]=max(nums[i]+a,dp[i-1]);
-        }
-        int b=dp[nums.size()-2];
+        if(nums.size()==2) return max(nums[0],nums[1]);
+
         dp[0]=nums[1];
-        for(int i=1;i<nums.size()-1;i++)
-        {
-           int a=0;
-           if(i<2) a=0;
-           else a=dp[i-2];
-           dp[i]=max(nums[i+1]+a,dp[i-1]);
+        dp[1]=max(nums[1],nums[2]);
+
+        for(int i=2;i<nums.size()-1;i++){
+            dp[i]=max(dp[i-2]+nums[i+1],dp[i-1]);
         }
-        return max(b,dp[nums.size()-2]);
+        int a=dp[nums.size()-2];
+
+        // leave last wala
+
+        vector<int>dp1(nums.size()-1,0);
+
+        dp1[0]=nums[0];
+        dp1[1]=max(nums[0],nums[1]);
+
+        for(int i=2;i<nums.size()-1;i++){
+            dp1[i]=max(dp1[i-2]+nums[i],dp1[i-1]);
+        }
+
+        int b=dp1[nums.size()-2];
+
+        return max(a,b);
+        
+
+
+
     }
 };
