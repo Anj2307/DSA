9268dac08af135bf6883bf927035178c57e1096b
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-07-18 10:19:29 +0530

    Time: 4 ms (55.35%) | Memory: 18.9 MB (26.98%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,32 +1,43 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-       vector<vector<int>>adj(numCourses);
+    vector<int> findOrder(int numCourses, vector<vector<int>>& pre) {
+        vector<vector<int>>vec(numCourses);
         vector<int>indegree(numCourses,0);
-        for(int i=0;i<prerequisites.size();i++)
+        vector<int>visited(numCourses,0);
+        for(auto i: pre)
         {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
-            indegree[prerequisites[i][0]]++;
+            vec[i[1]].push_back(i[0]);
+            indegree[i[0]]++;
         }
         queue<int>q;
-        for(int i=0;i<indegree.size();i++)
-        {
-            if(indegree[i]==0) q.push(i);
-        }
-        vector<int> topo;
-        while(!q.empty())
-        {
-            int n=q.front();q.pop();
-            topo.push_back(n);
-            for(auto it: adj[n])
+        int num=0;
+        for(int i=0;i<indegree.size();i++){
+            if(indegree[i]==0)
             {
-                indegree[it]--;
-                if(indegree[it]==0) q.push(it);
+                q.push(i);
+                num++;
+            }
+            
+        }
+        vector<int>v;
+       
+        while(!q.empty()){
+            int u= q.front(); q.pop();
+            v.push_back(u);
+            if(!visited[u]){
+                visited[u]=1;
+                for(auto i: vec[u])
+                {
+                    indegree[i]--;
+                    if(indegree[i]==0){
+                        num++;
+                        q.push(i);
+
+                    }
+                }
             }
         }
-        if(topo.size()<numCourses) return{};
-        return topo;
-        
-        
+        if(num!=numCourses) return {};
+       return v;
     }
 };
