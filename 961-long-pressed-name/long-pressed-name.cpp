f2fbb731d8ad0aca6e8baf9b0f28dd6746aa1cb0
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-07-27 22:05:21 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (99.83%) - LeetSync

--- 961-long-pressed-name/long-pressed-name.cpp
+++ 961-long-pressed-name/long-pressed-name.cpp
@@ -1,24 +1,44 @@
+#pragma GCC optimize("Ofast")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+static constexpr size_t max_align = alignof(max_align_t);
+alignas(max_align) static unsigned char BUFFER[64 * 1024 * 1024];
+static size_t pos = 0;
+
+void *operator new(const size_t size) {
+    const size_t padding = (max_align - (pos % max_align)) % max_align;
+    pos += padding + size;
+    return static_cast<void *>(&BUFFER[pos - size]);
+}
+
+void *operator new[](const size_t size) { return operator new(size); }
+void operator delete(void *) noexcept {}
+void operator delete[](void *) noexcept {}
+void operator delete(void *, size_t) noexcept {}
+void operator delete[](void *, size_t) noexcept {}
+
 class Solution {
 public:
+    Solution() {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
     bool isLongPressedName(string name, string typed) {
-        int i=0;
-        int j=0;
-        if(name[0]!=typed[0]) return false;
-        while(i<name.size() && j<typed.size()){
-            if (name[i]==typed[j]){
-                i++;
+        int n = name.size();
+        int j = 0;
+        if(j < n && name.size() > typed.size()) return false;
+        for(int i = 0; i < typed.size(); i++){
+            if(typed[i] == name[j]){
                 j++;
-            }
-            else if(typed[j]!=typed[j-1])
+            }else if( i > 0 && typed[i] == typed[i - 1]){
+                continue;
+            }else{
                 return false;
-            else j++;
-            
+            }
         }
-        if(i!=name.size()) return false;
-        for(int k=j;k<typed.size();k++){
-            if(typed[k]!=typed[j-1]) return false;
-        }
-        return true;
-        
+        return j == n;
     }
 };
