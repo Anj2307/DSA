54f6c447209ef89af66439fc758c9b18fe5c9b97
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-08-04 16:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (90.99%) - LeetSync

---
+++ 1008-binary-tree-cameras/binary-tree-cameras.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int cameras = 0;
+
+    
+    int dfs(TreeNode* root) {
+        if (root == nullptr) return 2; 
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        
+        if (left == 0 || right == 0) {
+            cameras++;
+            return 1;
+        }
+
+        
+        if (left == 1 || right == 1) {
+            return 2;
+        }
+
+        
+        return 0;
+    }
+
+public:
+    int minCameraCover(TreeNode* root) {
+        
+        if (dfs(root) == 0) {
+            cameras++;
+        }
+        return cameras;
+    }
+};
