58f0dbabb05de9ec83e23443f3682ec2914efad4
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-08-06 00:45:11 +0530

    Time: 98 ms (40.14%) | Memory: 61.9 MB (68.54%) - LeetSync

---
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        while(l<r){
+            if(nums[l]+nums[r]==target)
+                {
+                    ans++;
+                    l++;
+                    r--;
+                }
+
+            else if(nums[l]+nums[r]>target)
+                r--;
+            else l++;
+        }
+        return ans;
+    }
+};
