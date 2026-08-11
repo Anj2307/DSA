6099df1a4f41b8948cf7ddd03073f52919f96d48
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-08-11 22:58:52 +0530

    Time: 36 ms (85.63%) | Memory: 21 MB (56.06%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,44 +1,40 @@
 class Solution {
+private: 
+    int f(int i, int j1, int j2, vector<vector<int>>& grid, vector<vector<vector<int>>>& dp) {
+        int r_size = grid.size();
+        int c_size = grid[0].size();
+        
+        // Out of bounds check
+        if (j1 < 0 || j2 < 0 || j1 >= c_size || j2 >= c_size)
+            return -1e8;
+        
+        // Base case: last row
+        if (i == r_size - 1) {
+            if (j1 == j2) return grid[i][j1];
+            return grid[i][j1] + grid[i][j2];
+        }
+        
+        // Memoization check
+        if (dp[i][j1][j2] != -1) return dp[i][j1][j2];
+        
+        // Determine cherries collected at current step
+        int current_cherries = (j1 == j2) ? grid[i][j1] : (grid[i][j1] + grid[i][j2]);
+        
+        int ma = -1e8;
+        // Explore all 9 next state combinations
+        for (int r = -1; r <= 1; r++) {
+            for (int c = -1; c <= 1; c++) {
+                ma = max(ma, current_cherries + f(i + 1, j1 + r, j2 + c, grid, dp));
+            }
+        }
+        
+        return dp[i][j1][j2] = ma;
+    }
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        vector<vector<vector<int>>>dp(grid.size(),vector<vector<int>>(grid[0].size(),vector<int>(grid[0].size(),-1)));
-        int n=grid.size();
-        int m=grid[0].size();
-        for(int j=0;j<m;j++)
-        {
-            for(int k=0;k<m;k++)
-            {
-                if(j==k) dp[n-1][j][k]=grid[n-1][k];
-                else dp[n-1][j][k]=grid[n-1][k]+grid[n-1][j];
-            }
-        }
-        for(int i=n-2;i>=0;i--)
-        {
-            for(int j1=0;j1<m;j1++)
-            {
-                for(int j2=0;j2<m;j2++){
-                int maxi=-1e8;
-                for(int a=-1;a<2;a++)
-                {
-                    for(int b=-1;b<2;b++)
-                    {
-                        int value=0;
-                        if(j1==j2) value=grid[i][j1];
-                        else value=grid[i][j1]+grid[i][j2];
-                        if(j1+a>=0 && j1+a<m && j2+b>=0 && j2+b<m)
-                        {
-                            value+=dp[i+1][j1+a][j2+b];
-                        }
-                        else value=-1e8;
-                        maxi=max(value,maxi);
-                    }
-
-                }
-                dp[i][j1][j2]=maxi;
-            }
-            }
-
-        }
-        return dp[0][0][m-1];
+        int r_size = grid.size();
+        int c_size = grid[0].size();
+        vector<vector<vector<int>>> dp(r_size, vector<vector<int>>(c_size, vector<int>(c_size, -1)));
+        return f(0, 0, c_size - 1, grid, dp);   
     }
 };
