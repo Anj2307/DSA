5db829afd2456fcd06a5407934d0433bd5afc0a6
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-08-11 22:20:11 +0530

    Time: 196 ms (10.02%) | Memory: 22.9 MB (7.62%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,23 +1,17 @@
 class Solution {
+    private:
+        int f(vector<int>&dp,int i,vector<int>&nums){
+           if(i>=dp.size()-1) return 0;
+           if(dp[i]!=-1) return dp[i];
+           int min_value=1e9;
+           for(int j=1;j<=nums[i];j++){
+            min_value=min(min_value,1+f(dp,i+j,nums));
+           }
+           return dp[i]=min_value;
+        }
 public:
     int jump(vector<int>& nums) {
-        
-        vector<int>dp(nums.size(),-1);
-        dp[0]=0;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            for(int j=1;j<nums[i]+1;j++)
-            {
-                if(i+j<nums.size())
-                {
-                    if(dp[i+j]==-1){
-                        dp[i+j]=dp[i]+1;
-                    }else
-                        dp[i+j]=min(dp[i+j],dp[i]+1);
-                }
-            }
-        }
-        return dp[nums.size()-1];
+        vector<int>dp(nums.size(), -1);
+        return f(dp,0,nums);
     }
 };
