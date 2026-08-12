c20fcfa62125b6deeb0d0e8217eff5aaff5aa65e
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-08-12 19:49:03 +0530

    Time: 19 ms (74.21%) | Memory: 27.9 MB (72.75%) - LeetSync

---
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.cpp
@@ -0,0 +1,54 @@
+class Solution { 
+public: 
+    string repeatLimitedString(string s, int r) { 
+        priority_queue<int> p; 
+        vector<int> freq(26,0); 
+        for(auto i: s){ 
+            freq[i-'a']++; 
+        } 
+        for(int i=0;i<26;i++){ 
+            if(freq[i]>0){ 
+                p.push(i); 
+            } 
+        } 
+        int prev; 
+        bool flag=false; 
+        string result=""; 
+
+        while(!p.empty()){ 
+            int a=p.top(); 
+            p.pop(); 
+
+            if(flag){ 
+                // 'a' is our separator character because 'prev' is still waiting
+                result+=(a+'a'); 
+                freq[a]--; 
+
+                
+                p.push(prev); 
+
+                if(freq[a] > 0) {
+                    p.push(a);
+                }
+
+                flag=false; 
+            }else{ 
+                if(freq[a]<=r){ 
+                    while(freq[a]) { 
+                        result+=(a+'a'); 
+                        freq[a]--; 
+                    } 
+                    flag=false; 
+                }else{ 
+                    for(int i=0;i<r;i++){ 
+                        result+=(a+'a'); 
+                        freq[a]--; 
+                    } 
+                    flag=true; 
+                    prev=a; 
+                } 
+            } 
+        } 
+        return result; 
+    } 
+};
