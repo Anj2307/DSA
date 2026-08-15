3139578a45f71a465d938c2245f1e5583b442bc6
Anubhav Goyal <mc24bt005@iitdh.ac.in>
2026-08-16 01:14:19 +0530

    Time: 7 ms (98.11%) | Memory: 12.5 MB (5.49%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,31 +1,65 @@
 class Solution {
 private:
-    bool dfs(int i, int j, string& word, vector<vector<char>>& board, int index, vector<vector<bool>>& vis) {
-        if (index == word.length()) return true;
-        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size()) return false;
-        if (vis[i][j] || board[i][j] != word[index]) return false;
-
-        vis[i][j] = true;
-
+    bool f(vector<vector<char>>& board, string word,int i,int j, int r){
+        if(r==word.size()) return true;
+        if(i<0||i>=board.size()||j<0||j>=board[0].size()) return false;
         
-        bool found = dfs(i + 1, j, word, board, index + 1, vis) ||
-                     dfs(i - 1, j, word, board, index + 1, vis) ||
-                     dfs(i, j + 1, word, board, index + 1, vis) ||
-                     dfs(i, j - 1, word, board, index + 1, vis);
-
-        vis[i][j] = false; // backtrack
-        return found;
+        //dfs
+        vector<int>dr={-1,0,1,0};
+        vector<int>dc={0,-1,0,1};
+        if(board[i][j]==word[r]){
+            int ch=board[i][j];
+            board[i][j]='1';
+                for(int k=0;k<4;k++){
+                    if(f(board,word,dr[k]+i,dc[k]+j,r+1)){
+                        return true;
+                    }
+                }
+                board[i][j]=ch;
+            }
+            
+        
+        return false;
     }
-
+        
+    
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size(), n = board[0].size();
-        vector<vector<bool>> vis(m, vector<bool>(n, false));
+          int m = board.size();
+        int n = board[0].size();
+        if (word.size() > m * n) return false;
 
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (dfs(i, j, word, board, 0, vis))
+      
+        unordered_map<char, int> boardFreq;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boardFreq[board[i][j]]++;
+            }
+        }
+
+        
+        unordered_map<char, int> wordFreq;
+        for (char c : word) {
+            wordFreq[c]++;
+        }
+
+        
+        for (auto& pair : wordFreq) {
+            if (boardFreq[pair.first] < pair.second) {
+                return false;
+            }
+        }
+
+      
+        if (boardFreq[word.back()] < boardFreq[word.front()]) {
+            reverse(word.begin(), word.end());
+        }
+        for(int i=0;i<board.size();i++)
+        {
+            for(int j=0;j<board[i].size();j++){
+                if(f(board,word,i,j,0)){
                     return true;
+                }
             }
         }
         return false;
